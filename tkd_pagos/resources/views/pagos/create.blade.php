4a7cf767f40b11af165c4ad78fe3d525f51c5a85
KevinMartv <martinezmarinkevin@gmail.com>
2026-09-14 14:03:14 -0600

    fix: Corrección de formulario de tarjetas e integración exitosa con POS

--- tkd_pagos/resources/views/pagos/create.blade.php
+++ tkd_pagos/resources/views/pagos/create.blade.php
@@ -25,11 +25,11 @@
                     <input type="text" name="titular" value="{{ old('titular') }}" placeholder="Nombre como aparece en la tarjeta">
                 </label>
                 <label>Número
-                    <input type="text" name="numero" inputmode="numeric" placeholder="4242424242424242" maxlength="19">
+                    <input type="text" name="numero" value="{{ old('numero') }}" inputmode="numeric" placeholder="4242424242424242" maxlength="19">
                 </label>
                 <div class="pay-row">
                     <label>Vencimiento
-                        <input type="text" name="vencimiento" placeholder="MM/AA" maxlength="5">
+                        <input type="text" name="vencimiento" value="{{ old('vencimiento') }}" placeholder="MM/AA" maxlength="5">
                     </label>
                     <label>CVV
                         <input type="password" name="cvv" maxlength="4" autocomplete="off">
